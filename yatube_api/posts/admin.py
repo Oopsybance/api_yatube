cf8995d67b19764d7951eb27285220e0b16b920d
Oopsybance <108955282+Oopsybance@users.noreply.github.com>
2023-06-18 15:26:00 +0300

    Update admin.py

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -9,9 +9,11 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
