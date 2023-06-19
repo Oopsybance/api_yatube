2cc31fc210e93e6ca34989da446a8b00df732b63
Oopsybance <108955282+Oopsybance@users.noreply.github.com>
2023-06-19 12:03:43 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from django.shortcuts import get_object_or_404
 
-from posts.models import Post, Group
+from posts.models import Post, Group, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
@@ -32,8 +32,10 @@ class PostCommentsViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Возвращаем queryset комментариев для запрошенного поста."""
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
-        return post.comments.all().select_related('author')
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return Comment.objects.filter(post=post).select_related(
+            'author', 'post')
 
     def perform_create(self, serializer):
         """Установка автора и связанного поста при создании комментария."""
